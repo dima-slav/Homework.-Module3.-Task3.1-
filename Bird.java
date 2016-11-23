29c703859202921da36f067b98d01456d58fd1d6
Дима <dzirtdourden4@gmail.com>
2016-11-23 03:51:12 +0200

    Bird commmited

---
+++ Bird.java
@@ -0,0 +1,20 @@
+package Task3_1;
+
+/**
+ * Created by Дима on 23.11.2016.
+ */
+public class Bird {
+    String name;
+    String LivingArea;
+
+    public void sing(){
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am bird");
+    }
+    public Bird(String Name, String LivingArea){
+        this.name = name;
+        this.LivingArea = LivingArea;
+    }
+}
---
+++ BirdMain.java
@@ -0,0 +1,12 @@
+package Task3_1;
+
+/**
+ * Created by Дима on 23.11.2016.
+ */
+public class BirdMain {
+    public static void main(String[] args) {
+        Bird bird = new Bird("Swallow","Europe");
+
+        bird.sing();
+    }
+}
